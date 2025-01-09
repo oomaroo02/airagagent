76094a93f5a0cc3343a664e927add9c428b560cb
Marc Gueury <marc.gueury@oracle.com>
2025-01-09 08:30:16 +0100

    v3.0

--- apex/bin/ssh_compute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/..
-. env.sh
-
-eval "$(ssh-agent -s)"
-ssh-add $TF_VAR_ssh_private_path
-ssh -o StrictHostKeyChecking=no -oProxyCommand="$BASTION_PROXY_COMMAND" opc@$COMPUTE_IP
--- apex/src/terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "jdbc_url" {
-   value = local.jdbc_url
-}
-
-output "db_url" {
-   value = local.db_url
-}
