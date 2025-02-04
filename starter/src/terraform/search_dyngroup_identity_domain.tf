2ceb42e9402208fa6fbce9c405ab7b8f8fdc9bb6
Marc Gueury <marc.gueury@oracle.com>
2025-02-04 13:15:23 +0100

    v3.0

--- starter/src/terraform/search_dyngroup_identity_domain.tf
+++ starter/src/terraform/search_dyngroup_identity_domain.tf
@@ -7,6 +7,7 @@ resource "oci_identity_domains_dynamic_resource_group" "search-fn-dyngroup" {
     schemas = ["urn:ietf:params:scim:schemas:oracle:idcs:DynamicResourceGroup"]
 }
 
+/*
 resource "oci_identity_domains_dynamic_resource_group" "search-bastion-dyngroup" {
     #Required
     provider       = oci.home    
@@ -15,6 +16,7 @@ resource "oci_identity_domains_dynamic_resource_group" "search-bastion-dyngroup"
     matching_rule = "ALL {instance.id = '${oci_core_instance.starter_bastion.id}'}"
     schemas = ["urn:ietf:params:scim:schemas:oracle:idcs:DynamicResourceGroup"]
 }
+*/
 
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [ oci_identity_domains_dynamic_resource_group.search-bastion-dyngroup, oci_identity_domains_dynamic_resource_group.search-fn-dyngroup ]
@@ -30,6 +32,16 @@ resource "oci_identity_policy" "starter_search_policy" {
 
     statements = [
         "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-fn-dyngroup to manage objects in compartment id ${local.lz_serv_cmp_ocid}",
+        "allow any-user to manage all-resources in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${oci_core_instance.starter_bastion.id}",
+        "allow any-user to manage stream-family in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${oci_core_instance.starter_bastion.id}",
+        "allow any-user to manage genai-agent-family in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${oci_core_instance.starter_bastion.id}",
+        "allow any-user to manage genai-agent-family in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${data.oci_database_autonomous_database.starter_atp.autonomous_database_id}'",
+        "allow any-user to read object-family in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${data.oci_database_autonomous_database.starter_atp.autonomous_database_id}'",
+        "allow any-user to manage object-family in compartment id ${local.lz_serv_cmp_ocid} where ALL { request.principal.id='${data.oci_database_autonomous_database.starter_atp.autonomous_database_id}', request.permission = 'PAR_MANAGE' }"
+    ]
+    /*
+    statements = [
+        "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-fn-dyngroup to manage objects in compartment id ${local.lz_serv_cmp_ocid}",
         "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-bastion-dyngroup to manage all-resources in compartment id ${local.lz_serv_cmp_ocid}",
         "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-bastion-dyngroup to manage stream-family in compartment id ${local.lz_serv_cmp_ocid}",
         "allow dynamic-group ${var.idcs_domain_name}/${var.prefix}-bastion-dyngroup to manage genai-agent-family in compartment id ${local.lz_serv_cmp_ocid}",
@@ -37,5 +49,6 @@ resource "oci_identity_policy" "starter_search_policy" {
         "allow any-user to read object-family in compartment id ${local.lz_serv_cmp_ocid} where request.principal.id='${data.oci_database_autonomous_database.starter_atp.autonomous_database_id}'",
         "allow any-user to manage object-family in compartment id ${local.lz_serv_cmp_ocid} where ALL { request.principal.id='${data.oci_database_autonomous_database.starter_atp.autonomous_database_id}', request.permission = 'PAR_MANAGE' }"
     ]
+    */
 }
 
